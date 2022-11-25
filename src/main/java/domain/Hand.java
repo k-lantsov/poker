cdbfdcf1894bd7b4b7bfada22b2debacadd02b92
k.lantsov <k.lantsov@yahoo.com>
2022-11-25 21:16:31 +0300

    domain models init

--- src/main/java/domain/Hand.java
+++ src/main/java/domain/Hand.java
@@ -1,2 +1,18 @@
-package domain;public class Hand {
+package domain;
+
+public class Hand {
+
+    private Card[] cards;
+
+    public Hand(Card[] cards) {
+        this.cards = cards;
+    }
+
+    public Card[] getCards() {
+        return cards;
+    }
+
+    public void setCards(Card[] cards) {
+        this.cards = cards;
+    }
 }
